4ce159089ddf764e5d639c3283938b58c5b43188
Evgeny Tolmachev <evgeny.tolmachev@bjss.com>
2022-11-25 11:41:58 +0000

    Socket IO Dependancy

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {

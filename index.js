20020795339ffe3bd7c3b04721f9e6fb25b2588e
Evgeny Tolmachev <evgeny.tolmachev@bjss.com>
2022-11-25 12:21:45 +0000

    Chat Works

--- index.js
+++ index.js
@@ -5,13 +5,46 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Socket.IO is composed of two parts:
+// A server that integrates with (or mounts on) the Node.JS HTTP Server socket.io
+// A client library that loads on the browser side socket.io-client
+// During development, socket.io serves the client automatically for us, as we’ll see, so for now we only have to install one module:
+
+
+
+const homepage = __dirname + '/index.html'
+var number_of_users = 0;
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(homepage);
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    number_of_users++;
+    console.log('a user connected, total is  ' + number_of_users);
+    io.emit('chat message', "Greetings User" + number_of_users);
+    socket.on('disconnect', () => {
+        number_of_users--;
+      console.log('user disconnected, total is  ' + number_of_users);
+    });
 });
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+
+
+// This will emit the event to all connected sockets
+io.emit('connection', { someProperty: 'Hello User', otherProperty: 'other value' }); 
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
